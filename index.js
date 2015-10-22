190be39cc7c2aba2043f2975caba5ea15ab4ff09
SleepingWhale <vladimir@Vladimirs-MacBook-Air.local>
2015-10-22 17:08:40 +0300

    added comments

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 /************************************************************************************
-a-bunch - script lets you open multiple links at once.*
+Link-buncher
+Bookmark which allows to open multiple links at once.*
+
 installation: create new bookmark, copy and paste this code into bookmark link field.
+
 using:
+
 1. open web page
 2. press bookmark with code
 3. press and hold Shift
 4. move pointer over desirable links(they should get red frame)
-5. to open selected all selected links at once ckick any of red framed links
+5. release Shift
+6. to open all selected links at once ckick any of red framed links
 
-* assuming that popups are allowed for sites you work with in browser settings
+*- assuming that popups are allowed for sites you work with in browser settings
 ************************************************************************************/
 
 
@@ -18,7 +23,7 @@ javascript: (function() {
 
     var links = [];
 
-    function ahendler(event) {
+    function ahendler(event) {// makes red border for selected links, add link to links array
         if (event.shiftKey) {
             var elIndex = links.indexOf(this);
             if (elIndex === -1) {
@@ -26,7 +31,7 @@ javascript: (function() {
                 this.originalStyle = this.style.cssText;
                 this.style.border = "1px solid red";
                 this.addEventListener("click", opener, false);
-            } else {
+            } else {// on even mouseover for the same link undo style changes, removes from links array
                 links.splice(elIndex, 1);
                 this.style.cssText = this.originalStyle;
                 this.removeEventListener("click", opener, false);
@@ -34,7 +39,7 @@ javascript: (function() {
         }
     }
 
-    function opener(event) {
+    function opener(event) { // after clicking any red framed links opens all links in separate windows, remove all styles and event listener
         event.preventDefault();
         for (var i = 0, l = links.length; i < l; i++) {
             links[i].style.cssText = links[i].originalStyle;
